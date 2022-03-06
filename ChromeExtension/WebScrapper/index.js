b6e57d235567b6a15abea9ed9169c4b96fcdbef1
jm2601 <javier.miranda4420@gmail.com>
2022-03-05 22:28:03 -0800

    learning how to scape specific data

--- ChromeExtension/WebScrapper/index.js
+++ ChromeExtension/WebScrapper/index.js
@@ -4,9 +4,27 @@ const cheerio = require('cheerio')
 async function main() {
     let {data} = await axios.get('https://web-scraper-tutorial.netlify.app/')
     let $ = cheerio.load(data)
-    let text = $('div.col-12 > h2.favourite').text()
+    let text = $('div.col-12 > h2#favourite').text()
     //let text = $('div.container.bg-secondary > p.text-warning').text()
     console.log(text)
+
+/*
+loop through elements <p>
+
+let paragraphs = document.querySelectorAll('p')
+for(paragraph of paragraphs) {
+    let text = paragraph.innerText
+    console.log(text)
+}
+    
+jquery sample for console 
+
+let paragraphs = $('p')
+for(paragraph of paragraphs) {
+   let text = $(paragraph).text()
+   console.log(text)
+}
+*/
   }
   main()
   
