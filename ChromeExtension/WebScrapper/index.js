837187522a01a48ef134f85ed17cb83d78159dee
jm2601 <javier.miranda4420@gmail.com>
2022-03-05 22:09:39 -0800

    messing around with the webscraper and adjusting what elements to search for

--- ChromeExtension/WebScrapper/index.js
+++ ChromeExtension/WebScrapper/index.js
@@ -4,7 +4,8 @@ const cheerio = require('cheerio')
 async function main() {
     let {data} = await axios.get('https://web-scraper-tutorial.netlify.app/')
     let $ = cheerio.load(data)
-    let text = $('div.container.bg-secondary > p.text-warning').text()
+    let text = $('div.col-12 > h2.favourite').text()
+    //let text = $('div.container.bg-secondary > p.text-warning').text()
     console.log(text)
   }
   main()
