04f0047ca61dad95b429b3fd69c571af22b6afa8
jm2601 <javier.miranda4420@gmail.com>
2022-03-05 23:50:34 -0800

    modified webscrapper using Express.js and scraped html data from test website

--- ChromeExtension/WebScrapper/index.js
+++ ChromeExtension/WebScrapper/index.js
@@ -1,13 +1,29 @@
+const PORT = 8000
+
 const axios = require('axios')
 const cheerio = require('cheerio')
+const express = require('express')
 
-async function main() {
+const app = express()
+
+const url = "https://web-scraper-tutorial.netlify.app/"
+
+axios(url)
+    .then(repsonse => {
+        const html = response.data
+        console.log(html)
+    })
+
+app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
+
+
+/*async function main() {
     let {data} = await axios.get('https://web-scraper-tutorial.netlify.app/')
     let $ = cheerio.load(data)
     let text = $('div.col-12 > h2#favourite').text()
     //let text = $('div.container.bg-secondary > p.text-warning').text()
     console.log(text)
-
+*/
 /*
 loop through elements <p>
 
@@ -24,7 +40,8 @@ for(paragraph of paragraphs) {
    let text = $(paragraph).text()
    console.log(text)
 }
-*/
+
   }
+
   main()
-  
+*/
